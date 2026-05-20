2885e3fd82730b33916cc173d153e136ec2b9567
Rivu Layek <rivulayek15@gmail.com>
2026-05-20 22:57:46 +0530

    Inject __APP_ENV__ definition in vite.config.js for Vercel environments

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(process.env.VITE_VERCEL_ENV || process.env.VERCEL_ENV),
+    },
+  };
+});
